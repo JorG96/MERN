c0558f738b1b94e356efa7c2d8a6383ec64535ec
JorG96 <jecamg96@gmail.com>
2021-09-22 16:55:00 -0400

    frontend added

--- restaurant-reviews/backend/index.js
+++ restaurant-reviews/backend/index.js
@@ -1,17 +1,28 @@
-import express from "express"
-import RestaurantsCtrl from "./api/restaurants.controller.js"
-import ReviewsCtrl from "./api/reviews.controller.js"
+import app from "./server.js"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
+import ReviewsDAO from "./dao/reviewsDAO.js"
+dotenv.config()
+const MongoClient = mongodb.MongoClient
 
-const router = express.Router()
+const port = process.env.PORT || 8000
 
-router.route("/").get(RestaurantsCtrl.apiGetRestaurants)
-router.route("/id/:id").get(RestaurantsCtrl.apiGetRestaurantById)
-router.route("/cuisines").get(RestaurantsCtrl.apiGetRestaurantCuisines)
-
-router
-  .route("/review")
-  .post(ReviewsCtrl.apiPostReview)
-  .put(ReviewsCtrl.apiUpdateReview)
-  .delete(ReviewsCtrl.apiDeleteReview)
-
-export default router
+MongoClient.connect(
+  process.env.RESTREVIEWS_DB_URI,
+  {
+    poolSize: 50,
+    wtimeout: 2500,
+    useNewUrlParse: true }
+  )
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    await RestaurantsDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client)
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
+  })
